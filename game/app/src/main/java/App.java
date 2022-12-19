39ee1a152626b3bc2866f485144597293e667bac
ThotuB <59542225+ThotuB@users.noreply.github.com>
2022-12-19 19:21:16 +0200

    added statistics

---
+++ game/app/src/main/java/App.java
@@ -0,0 +1,66 @@
+import game.Game;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+public class App {
+    public static void Send() {
+        var factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare("HELLO", false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", "HELLO", null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+        catch (Exception ex){
+            System.out.println(ex.toString());
+        }
+    }
+
+    public static void Receive() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        System.out.println("connection created");
+        Channel channel = connection.createChannel();
+        System.out.println("channel created");
+
+        channel.queueDeclare("HELLO", false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback callback = ((consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        });
+
+        channel.basicConsume("HELLO", true, callback, consumerTag -> { });
+
+    }
+
+    public static void main(String[] args) {
+//        Game.Config config = new Game.Config(
+//            10,
+//            10,
+//            100
+//        );
+//
+//        Game game = new Game(config);
+//
+//        game.simulate();
+
+        System.out.println("Starting message receiver stub");
+
+        try {
+            Receive();
+        } catch (Exception e) {
+            System.out.println("Registering RX handler failed " + e);
+        }
+
+    }
+}
--- sample_rabbitmq/run.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\Vlad\.jdks\corretto-17.0.5\bin\java -cp .;amqp-client-5.7.1.jar;slf4j-api-1.7.26.jar;slf4j-simple-1.7.26.jar Driver
